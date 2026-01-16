07a23a91da2fad05d70ae8daa9af995c73e82037
arsenopyrite <arsenopyrite@gmail.com>
2026-01-16 01:19:23 +0000

    [A125上午9:1916C1619i上午9:19] Jan

--- src/main/java/com/example/htmlmud/domain/actor/RoomActor.java
+++ src/main/java/com/example/htmlmud/domain/actor/RoomActor.java
@@ -16,6 +16,10 @@ public class RoomActor {
 // // 雖然 PlayerActor 是物件，但 Key 是 primitive long，這能避免大量 Long 物件的拆裝箱
 // private final Long2ObjectOpenHashMap<PlayerActor> playersInRoom = new Long2ObjectOpenHashMap<>();
 
+// 使用 Composite ID 作為 Key
+// 這樣 Player 1 和 Mob 1 就不會衝突了！
+// private final Map<GameObjectId, LivingActor> inhabitants = new HashMap<>();
+
 // // 存放地上的物品 (Item ID -> ItemRecord)
 // private final Long2ObjectOpenHashMap<ItemRecord> itemsOnFloor = new Long2ObjectOpenHashMap<>();
 
@@ -26,4 +30,12 @@ public class RoomActor {
 // }
 
 // // ... handleMessage 邏輯 ...
+// private void handleAttack(GameObjectId attackerId, GameObjectId targetId) {
+// LivingActor target = inhabitants.get(targetId);
+// if (target != null) {
+// // 多型 (Polymorphism) 的威力：
+// // 不管是 Player 還是 Mob，都呼叫同一個方法
+// target.takeDamage(10, attackerId);
+// }
+// }
 // }
