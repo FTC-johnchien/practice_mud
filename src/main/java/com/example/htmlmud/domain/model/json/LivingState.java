279c1f5fbcbaac5258beb30bb0f551493be07c1b
arsenopyrite <arsenopyrite@gmail.com>
2026-01-16 00:38:19 +0000

    [A258上午8:3816C1638i上午8:38] Jan

--- src/main/java/com/example/htmlmud/domain/model/json/LivingState.java
+++ src/main/java/com/example/htmlmud/domain/model/json/LivingState.java
@@ -1,5 +1,26 @@
 package com.example.htmlmud.domain.model.json;
 
-public class LivingState {
+import java.util.HashMap;
+import java.util.Map;
 
+// 這個物件會被序列化存入 players.state_json 和 mobs.state_json
+public class LivingState {
+  public String name;
+  public String displayName;
+  public int level = 1;
+  public int hp;
+  public int maxHp;
+  public int mp;
+  public int maxMp;
+
+  // 基礎屬性
+  public Map<String, Integer> attributes = new HashMap<>(); // STR, DEX, INT
+
+  // 戰鬥狀態 (不存入 DB，或是標記 @JsonIgnore)
+  public transient boolean isDead = false;
+
+  // 建構子與輔助方法...
+  public boolean isDead() {
+    return hp <= 0;
+  }
 }
